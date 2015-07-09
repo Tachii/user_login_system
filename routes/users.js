491a67eecb3ebed8199bf5d158ea4d0ba0a12fa7
Tachi <zaveruha007@gmail.com>
2015-07-09 17:26:55 +0300

    Form Validation

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,69 @@ router.get('/login', function(req, res, next) {
   });
 });
 
+router.post('/register', function(req, res, next) {
+  //Get Form Values
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  //Check for Image Field
+  if (req.files.profileimage) {
+    console.log('Uploading File...');
+    //File Info
+    var profileImageOriginalName = req.files.profileimage.originalname;
+    var profileImageName = req.files.profileimage.name;
+    var profileImageMime = req.files.profileimage.mimeType;
+    var profileImagePath = req.files.profileimage.path;
+    var profileImageExt = req.files.profileimage.extension;
+    var profileImageSize = req.files.profileimage.Size;
+  } else {
+    //Set a Default Image
+    var profileImageName = 'default.png';
+  }
+
+  //Form Validation
+  req.checkBody('name', 'Name filed is required').notEmpty();
+  req.checkBody('email', 'Email filed is required').isEmail();
+  req.checkBody('username', 'Username filed is required').notEmpty();
+  req.checkBody('password', 'Password filed is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  //Check for errors
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render('register', {
+      errorss: errors,
+      name: name,
+      email: email,
+      username: username,
+      passowrd: password,
+      password2: password2
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      passowrd: password,
+      profileimage: profileImageName
+    });
+
+    //Create User
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err;
+      console.log(user);
+    });
+
+    //Sending Flash Message to User
+    req.flah('success', 'You are now registered and may login');
+    //Redirect back to home profileimage
+    res.location('/');
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
